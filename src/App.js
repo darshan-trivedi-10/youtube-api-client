6a9bf208dc768c3246ce93e159854602e0199cc2
Darshan Trivedi <trivedidarshan30@gmail.com>
2023-06-30 15:57:23 +0530

    folder structure setup + tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Home />
+    </>
   )
 }
 
-export default App
+export default App;
